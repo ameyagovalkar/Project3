b4afe6dd5cb2749cf6f226a58e449b4ebce17c58
ameyagovalkar <36211515+ameyagovalkar@users.noreply.github.com>
2018-04-26 23:11:42 -0700

    Update Catalog.cpp

--- Catalog.cpp
+++ Catalog.cpp
@@ -13,35 +13,103 @@
 
 using namespace std;
 
+
 bool	Catalog::AddCategory(uint64_t categoryNumber, const string& name)
 {
 	// to be completed
-myCategory[categoryNumber].cName = name;
-	return true;
+
+
+	if (category[categoryNumber] == category[categoryNumber])
+	{
+		return false;
+	}
+	else
+	{
+		Category temp;
+		temp.name = name;
+		temp.num = categoryNumber;
+
+		category[temp.num] = temp;
+		cNum++;
+
+		return true;
+
+
+	}
+
+	
 }
 
 bool	Catalog::AddProduct(uint64_t categoryNumber, uint64_t productNumber, const string& name)
 {
 	// to be completed
-	return true;
+
+	if (category[categoryNumber].product[productNumber]== category[categoryNumber].product[productNumber])
+	{
+		return false;
+	}
+	else
+	{
+		category[categoryNumber].product[productNumber] = name;
+		pNum++;
+		return true;
+		
+	}
 }
 
 uint64_t	Catalog::GetCategoryCount()
 {
 	// to be completed
-	return 0;
+
+	return category.size();
+
 }
 
 int64_t	Catalog::GetProductCount(uint64_t categoryNumber)
 {
 	// to be completed
-	return 1;
+	return category[categoryNumber].product.size();
 }
 
 bool	Catalog::Load(const string& fileName)
 {
 	// to be completed
-	return true;
+
+	ifstream inputFile;
+	inputFile.open(fileName);
+	uint64_t catNum, prodNum;
+	string catName, prodName, temp;
+
+	if (!inputFile)
+	{
+		return false;
+	}
+
+	while (inputFile)
+	{
+
+		inputFile >> temp;
+		if (temp == "Category")
+		{
+			inputFile >> catNum;
+			getline(inputFile, catName, '\n');
+			AddCategory(catNum, catName);
+		}
+		else
+		{
+			istringstream(temp) >> prodNum;
+			getline(inputFile, prodName, '\n');
+			AddProduct(catNum, prodNum, prodName);
+		}
+
+		if (inputFile.eof() == true)
+		{
+			inputFile.close();
+			return true;
+		}
+
+	}
+	return false;
 }
 
 //****************************************************************************************
@@ -61,18 +129,25 @@ bool	Catalog::Load(const string& fileName)
 bool	Catalog::ShowProduct(ostream& stream, uint64_t categoryNumber, uint64_t productNumber)
 {
 	// to be completed
+
+
+
 	return true;
 }
 
 bool	Catalog::ShowCategory(ostream& stream, uint64_t categoryNumber)
 {
 	// to be completed
+
+
 	return true;
 }
 
 bool	Catalog::ShowAll(ostream& stream)
 {
 	// to be completed
+
+
 	return true;
 }
 
